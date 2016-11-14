d85b0fde6990ee33d615fb6d10a6218ccf060b61
KamalAwasthi <kamalawasthi97@gmail.com>
2016-11-14 14:18:26 +0530

    messgae

--- location_assist/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import SaveSettings,Live,Reminder,FriendList,UserSummary
-
-admin.site.register(SaveSettings)
-admin.site.register(Live)
-admin.site.register(Reminder)
-admin.site.register(FriendList)
-admin.site.register(UserSummary)
